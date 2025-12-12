a9e95beaf7b8fd7c3015b45b50e547eb288b2c8c
mauricioandre10-ctrl/ <mauricioandre10@hotmail.com>
2025-12-12 13:19:49 +0100

    frist commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Layout/Header";
+import Footer from "@/components/Layout/Footer";
+import { ThemeProvider } from "next-themes";
+import SessionProviderComp from "@/components/nextauth/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+import { AuthDialogProvider } from "./context/AuthDialogContext";
+import ScrollToTop from "@/components/ScrollToTop";
+const dmsans = DM_Sans({ subsets: ["latin"] });
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await getServerSession(authOptions);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${dmsans.className}`}>
+        <AuthDialogProvider>
+          <SessionProviderComp session={session}>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={false}
+              defaultTheme="light"
+            >
+              <Header />
+              {children}
+              <Footer />
+              <ScrollToTop />
+            </ThemeProvider>
+          </SessionProviderComp>
+        </AuthDialogProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,35 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+
+export const authOptions: AuthOptions = {
+    // site: process.env.NEXTAUTH_URL || 'http://localhost:3000', // Managed by NEXTAUTH_URL env var
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || "",
+        }),
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                username: { label: 'Username', type: 'text' },
+                password: { label: 'Password', type: 'password' },
+            },
+            authorize: async (credentials) => {
+                // Add your own authentication logic here
+                if (credentials?.username === 'admin' && credentials?.password === 'admin123') {
+                    // Return user object if credentials are valid
+                    return { id: "1", name: 'Admin', email: 'admin@example.com' };
+                } else {
+                    // Return null if credentials are invalid
+                    return null;
+                }
+            },
+        }),
+    ],
+};
